a51ba60143df7eea20cc4f140d129baee854ae7a
Emran <63017060+Emrandotcom@users.noreply.github.com>
2021-10-12 21:35:32 -0400

    Uploaded java files

---
+++ Double Linked Lists/BasicDoubleLinkedList.java
@@ -0,0 +1,39 @@
+
+public class BasicDoubleLinkedList<T> extends java.lang.Object implements java.lang.Iterable<T> {
+	public BasicDoubleLinkedList() {
+
+	}
+
+	public int getSize() {
+		int sampleInt = 3;
+		return sampleInt;
+	}
+
+	public BasicDoubleLinkedList<T> addToEnd(T data) {
+	}
+
+	public BasicDoubleLinkedList<T> addToFront(T data) {
+	}
+
+	public T getFirst() {
+	}
+
+	public T getLast() {
+	}
+
+	public java.util.ListIterator<T> iterator()
+			throws java.lang.UnsupportedOperationException, java.util.NoSuchElementException {
+	}
+
+	public BasicDoubleLinkedList<T> remove(T targetData, java.util.Comparator<T> comparator) {
+	}
+
+	public T retrieveFirstElement() {
+	}
+
+	public T retrieveLastElement() {
+	}
+
+	public java.util.ArrayList<T> toArrayList() {
+	}
+}
---
+++ Double Linked Lists/SortedDoubleLinkedList.java
@@ -0,0 +1,20 @@
+
+public class SortedDoubleLinkedList<T> extends BasicDoubleLinkedList<T> {
+	public SortedDoubleLinkedList(java.util.Comparator<T> comparator2) {
+	}
+
+	public SortedDoubleLinkedList<T> add(T data) {
+	}
+
+	public BasicDoubleLinkedList<T> addToEnd(T data) throws java.lang.UnsupportedOperationException {
+	}
+
+	public BasicDoubleLinkedList<T> addToFront(T data) throws java.lang.UnsupportedOperationException {
+	}
+
+	public java.util.ListIterator<T> iterator() {
+	}
+
+	public SortedDoubleLinkedList<T> remove(T data, java.util.Comparator<T> comparator) {
+	}
+}
